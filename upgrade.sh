621730fae12535ecf58499fac663e3f94e5e1ada
沉银 <xinsheng.zxs@alibaba-inc.com>
2020-08-27 00:15:00 +0800

    add code

--- upgrade.sh
+++ upgrade.sh
@@ -1,9 +1,24 @@
+#!/bin/bash
 # 每个应用都重新打包, 并发送到远程的阿里云镜像仓库
 
 docker login --username=xinsheng.zxs@alibaba-inc.com --password=a8263099  registry.cn-hangzhou.aliyuncs.com
 
-#account_service=./account_service
+build_package() {
+  echo $1
+  cd ./$1
+  mvn clean package -DskipTests
+  cd ..
+  docker build -t $1:1.0 ./$1
+  docker tag $1:1.0 registry.cn-hangzhou.aliyuncs.com/xinsheng/$1:1.0
+  docker push registry.cn-hangzhou.aliyuncs.com/xinsheng/$1:1.0
+}
 
-docker build -t account-service:1.0 ./account_service
-docker tag account-service:1.0 registry.cn-hangzhou.aliyuncs.com/account-service:1.0
-
+build_package 'account-service'
+build_package 'auth-service'
+build_package 'config'
+build_package 'gateway'
+build_package 'monitoring'
+build_package 'notification-service'
+build_package 'registry'
+build_package 'statistics-service'
+build_package 'turbine-stream-service'
